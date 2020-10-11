453db946f2d01c063f45d2a3c3d67d8d52f23a25
Dwayne Morgan <dwaynerowayne@gmail.com>
2020-10-11 17:15:40 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,167 @@
 
-"use strict";
-window.onload= function addClassName()
-{
-let box = document.querySelectorAll('#board div');
-for (let i=0; i<box.length; i++){
-box[i].classList.add("square");
-console.log;
+window.onload= function board(){
+
+box = document.getElementById("board").children;
+
+for ( let i=0; i<box.length;i++){
+	box[i].classList.add("class","square");	
+	currentPlayer();
+	hover();
+	}
+
 }
+
+
+
+let W = [];
+
+
+function currentPlayer() {
+    let box = document.querySelectorAll("#board div");
+    for (let i = 0; i < box.length; i++) {
+        box[i].onclick = function() {
+            if(W.length === 0 && box[i].innerHTML === "") {
+                box[i].classList.add("square", "X");
+                box[i].innerHTML = "X";
+                W.push("X");
+
+                checkWinner();
+                restart();
+
+
+
+            }
+            else {
+                if (W[W.length - 1] === "X" && box[i].innerHTML === ""){
+                    box[i].classList.add("square", "O");
+                    box[i].innerHTML = "O";
+                    W.push("O");
+
+                    checkWinner();
+                    restart();
+
+
+
+                }
+                else if (W[W.length - 1] === "O" && box[i].innerHTML === ""){
+                    box[i].classList.add("square", "X");
+                    box[i].innerHTML = "X";
+                    W.push("X");
+
+                    checkWinner();
+                    restart();
+
+
+
+
+                }
+            }
+        }
+    }
+}
+
+
+function hover() {
+    let box = document.getElementById("board").children;
+
+    for (let i = 0; i < box.length; i++) {
+        box[i].onmouseover = function() {
+
+            box[i].classList.toggle("hover", true)
+
+        }
+        box[i].onmouseout = function() {
+
+            box[i].classList.toggle("hover", false)
+
+        }
+    }
+
+}
+
+function restart() {
+    let squares = document.querySelectorAll("#board div");
+    document.getElementsByClassName("btn")[0].addEventListener("click", function() {
+        game.length = 0;
+        for (let j= 0; j < squares.length; j++) {
+            squares[j].innerHTML = "";
+        }
+
+     	document.getElementById("status").innerHTML = "Move your mouse over a square and click to play an X or an O.";
+        document.getElementById("status").classList.remove("you-won");
+    });
+}
+
+function checkWinner() {
+    let box = document.getElementById("board").children;
+
+     if (box[0].textContent !== "" && box[0].textContent === "O" && box[1].textContent === "O" && box[2].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[3].textContent !== "" && box[3].textContent === "O" && box[4].textContent === "O" && box[5].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[6].textContent !== "" && box[6].textContent === "O" && box[7].textContent === "O" && box[8].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[2].textContent !== "" && box[2].textContent === "O" && box[4].textContent === "O" && box[6].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[0].textContent !== "" && box[0].textContent === "O" && box[4].textContent === "O" && box[8].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[0].textContent === "O" && box[0].textContent !== "" && box[3].textContent === "O" && box[6].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[1].textContent !== "" && box[1].textContent === "O" && box[4].textContent === "O" && box[7].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+     if (box[2] !== "" && box[2].textContent === "O" && box[5].textContent === "O" && box[8].textContent === "O") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+    }
+    if (box[0].textContent !== "" && box[0].textContent === "X" && box[1].textContent === "X" && box[2].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[3].textContent !== "" && box[3].textContent === "X" && box[4].textContent === "X" && box[5].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+
+     if (box[3].textContent !== "" && box[3].textContent === "X" && box[4].textContent === "X" && box[5].textContent === "X") {
+        update.classList.add("status.you-won::before", "status", "you-won", "#status.you-won::after");
+        update.innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[6].textContent !== "" && box[6].textContent === "X" && box[7].textContent === "X" && box[8].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[2].textContent !== "" && box[2].textContent === "X" && box[4].textContent === "X" && box[6].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[0].textContent !== "" && box[0].textContent === "X" && box[4].textContent === "X" && box[8].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[0].textContent !== "" && box[0].textContent === "X" && box[3].textContent === "X" && box[6].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[1].textContent !== "" && box[1].textContent === "X" && box[4].textContent === "X" && box[7].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
+     if (box[2].textContent !== "" && box[2].textContent === "X" && box[5].textContent === "X" && box[8].textContent === "X") {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+    }
 }
